98a41c81040c0bb7b546d736ebe553305e3fbcb6
Stephen O <stephen.oveson@gmail.com>
2020-04-07 19:15:31 -0600

    Updates

---
+++ precache-manifest.b07f8ceda5851465a75dbe0c83872a32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be0cbd67b28a7ac540aab830613dbcdc",
+    "url": "/leagueofpatches/index.html"
+  },
+  {
+    "revision": "0bf98bac8505175090c1",
+    "url": "/leagueofpatches/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90807ec80cbafa302aa7",
+    "url": "/leagueofpatches/static/js/2.ae0a2358.chunk.js"
+  },
+  {
+    "revision": "bc51ad6b075e5b96cf9ad2ad3d22ac0d",
+    "url": "/leagueofpatches/static/js/2.ae0a2358.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bf98bac8505175090c1",
+    "url": "/leagueofpatches/static/js/main.a4eaebc5.chunk.js"
+  },
+  {
+    "revision": "37294865da7aa3e13c07",
+    "url": "/leagueofpatches/static/js/runtime-main.da037e1d.js"
+  }
+]);
