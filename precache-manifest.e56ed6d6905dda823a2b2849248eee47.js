ded1cf53393511609815014af0de5a66c1f00a36
Stephen O <stephen.oveson@gmail.com>
2020-04-18 11:31:24 -0600

    Updates

--- precache-manifest.e56ed6d6905dda823a2b2849248eee47.js
+++ precache-manifest.e56ed6d6905dda823a2b2849248eee47.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5681c570b4ffa04ca6b4c713ea249e3e",
+    "revision": "9081864f4f1f30937482c756901d73b8",
     "url": "/LeagueOfPatches/index.html"
   },
   {
-    "revision": "7e8722eaf3c70c4a63e0",
+    "revision": "38a2a2f9a76e3bb9f5c6",
     "url": "/LeagueOfPatches/static/css/main.c8953361.chunk.css"
   },
   {
-    "revision": "7de2dfd85a2d7aff8198",
-    "url": "/LeagueOfPatches/static/js/2.3fbff944.chunk.js"
+    "revision": "bc0ebbe39708cfee85f4",
+    "url": "/LeagueOfPatches/static/js/2.f66083ff.chunk.js"
   },
   {
     "revision": "bc51ad6b075e5b96cf9ad2ad3d22ac0d",
-    "url": "/LeagueOfPatches/static/js/2.3fbff944.chunk.js.LICENSE.txt"
+    "url": "/LeagueOfPatches/static/js/2.f66083ff.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7e8722eaf3c70c4a63e0",
-    "url": "/LeagueOfPatches/static/js/main.970b0ba5.chunk.js"
+    "revision": "38a2a2f9a76e3bb9f5c6",
+    "url": "/LeagueOfPatches/static/js/main.81f34250.chunk.js"
   },
   {
     "revision": "d5e82e8991387f7c0793",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/LeagueOfPatches/precache-manifest.77e8832f87a7273d5c5fc57579895d81.js"
+  "/LeagueOfPatches/precache-manifest.e56ed6d6905dda823a2b2849248eee47.js"
 );
 
 self.addEventListener('message', (event) => {
